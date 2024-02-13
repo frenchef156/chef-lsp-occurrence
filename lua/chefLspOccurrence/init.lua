208e24e2bf0ee7622dd7fc234f46e0cd0d353edd
frenchef156 <frenchef156@users.noreply.github.com>
2024-02-13 03:17:56 +0200

    Fix wrap and improve highlights reload (#1)
     - Fix wrapping around of jump to occurrence
     - Remove CursorMoved and rely only on CursorHold
       - Clear the highlights only on next CursorHold (instead of on move)

--- lua/chefLspOccurrence/init.lua
+++ lua/chefLspOccurrence/init.lua
@@ -1,21 +1,34 @@
 local ChefLspOccurrence = {}
 
+ChefLspOccurrence.didJump = false
+
+local function performJump(highlightData)
+	ChefLspOccurrence.didJump = true
+	vim.api.nvim_win_set_cursor(0, {highlightData[2] + 1, highlightData[3] + 1}) --lsp index is zero based, vim is 1 based
+end
+
+local function isLsp(highlightData)
+	return highlightData[4].hl_group == "LspReferenceText" or highlightData[4].hl_group == "LspReferenceRead" or highlightData[4].hl_group == "LspReferenceWrite"
+end
+
 -- Go to the next occurrence of the highlighted identifier
 function ChefLspOccurrence.next()
 	local highlights = vim.api.nvim_buf_get_extmarks(0, -1, 0, -1, {details = true})
 	local foundNext = false
 	for i,v in ipairs(highlights) do
-		local is_lsp = v[4].hl_group == "LspReferenceText" or v[4].hl_group == "LspReferenceRead" or v[4].hl_group == "LspReferenceWrite"
-		if is_lsp and v[2] + 1 > vim.api.nvim_win_get_cursor(0)[1] then -- V[2] is the line number
-			vim.api.nvim_win_set_cursor(0, {v[2] + 1, v[3] + 1}) --lsp index is zero based, vim is 1 based
+		if isLsp(v) and v[2] + 1 > vim.api.nvim_win_get_cursor(0)[1] then -- V[2] is the line number
+			performJump(v)
 			foundNext = true
 			break
 		end
 	end
 	if not foundNext then
 		-- Wrapped around, go to the first one
-		if #highlights > 0 then
-			vim.api.nvim_win_set_cursor(0, {highlights[1][2] + 1, highlights[1][3] + 1}) --lsp index is zero based, vim is 1 based
+		for i,v in ipairs(highlights) do
+			if isLsp(v) then
+				performJump(v)
+				break
+			end
 		end
 	end
 end
@@ -25,19 +38,29 @@ function ChefLspOccurrence.prev()
 	local highlights = vim.api.nvim_buf_get_extmarks(0, -1, 0, -1, {details = true})
 	local foundPrev = false
 	for i=#highlights,1,-1 do
-		local is_lsp = highlights[i][4].hl_group == "LspReferenceText" or highlights[i][4].hl_group == "LspReferenceRead" or highlights[i][4].hl_group == "LspReferenceWrite"
-		if is_lsp and highlights[i][2] + 1 < vim.api.nvim_win_get_cursor(0)[1] then -- V[2] is the line number
-			vim.api.nvim_win_set_cursor(0, {highlights[i][2] + 1, highlights[i][3] + 1}) --lsp index is zero based, vim is 1 based
+		if isLsp(highlights[i]) and highlights[i][2] + 1 < vim.api.nvim_win_get_cursor(0)[1] then -- V[2] is the line number
+			performJump(highlights[i])
 			foundPrev = true
 			break
 		end
 	end
 	if not foundPrev then
 		-- Wrapped around, go to the last one
-		if #highlights > 0 then
-			vim.api.nvim_win_set_cursor(0, {highlights[#highlights][2] + 1, highlights[#highlights][3] + 1}) --lsp index is zero based, vim is 1 based
+		for i=#highlights,1,-1 do
+			if isLsp(highlights[i]) then
+				performJump(highlights[i])
+				break
+			end
 		end
 	end
+end
+
+local function onCursorHold(ev)
+	if not ChefLspOccurrence.didJump then
+		vim.lsp.buf.clear_references()
+		vim.lsp.buf.document_highlight()
+	end
+	ChefLspOccurrence.didJump = false
 end
 
 function ChefLspOccurrence.setup()
@@ -49,22 +72,18 @@ function ChefLspOccurrence.setup()
 			if client.server_capabilities.documentHighlightProvider then
 				vim.api.nvim_create_autocmd('CursorHold', {
 					group = chefLspGroup,
-					callback = function(ev)
-						vim.lsp.buf.document_highlight()
-					end
+					callback = onCursorHold
 				})
 				vim.api.nvim_create_autocmd('CursorHoldI', {
 					group = chefLspGroup,
-					callback = function(ev)
-						vim.lsp.buf.document_highlight()
-					end
+					callback = onCursorHold
 				})
-				vim.api.nvim_create_autocmd('CursorMoved', {
-					group = chefLspGroup,
-					callback = function(ev)
-						vim.lsp.buf.clear_references()
-					end
-				})
+				-- vim.api.nvim_create_autocmd('CursorMoved', {
+				-- 	group = chefLspGroup,
+				-- 	callback = function(ev)
+				-- 		vim.lsp.buf.clear_references()
+				-- 	end
+				-- })
 			end
 		end,
 	})
